802d558865345037f6459df5806e8175ec72d98f
ngochai9a7 <ngochai9a7@gmail.com>
2023-05-07 14:33:39 +0700

    data fetching

---
+++ pages/events/[cat]/index.js
@@ -0,0 +1,16 @@
+function Page() {
+    return (
+        <div>
+            <h1>Event in London</h1>
+            <h1 href="/events/event1">Event 1 </h1>
+            <h1 href="/events/event2">Event 2 </h1>
+            <h1 href="/events/event3">Event 3 </h1>
+            <h1 href="/events/event4">Event 4 </h1>
+            <h1 href="/events/event5">Event 5 </h1>
+            <h1 href="/events/event6">Event 6 </h1>
+
+        </div>
+        
+    )
+}
+export default Page;
